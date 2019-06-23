37da17108a7a7b40eea684bfa421e70709f39c90
Asus <a.ekush045@gmail.com>
2019-06-24 04:22:29 +0600

    Initial commit

---
+++ app/src/main/java/com/nullstdio/bbfc2020/MainActivity.java
@@ -0,0 +1,42 @@
+package com.nullstdio.bbfc2020;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView bbfcIv ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bbfcIv = (ImageView) findViewById(R.id.splashIv);
+
+        Animation myanim = AnimationUtils.loadAnimation(this , R.anim.prontpageanim);
+        bbfcIv.setAnimation(myanim);
+
+       final Intent intent = new Intent(this , MainMainActivity.class);
+
+        Thread timer = new Thread(){
+            public void run(){
+                try {
+                    sleep(4200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    startActivity(intent);
+                    finish();
+                }
+
+            }
+        };
+        timer.start();
+    }
+}
---
+++ app/src/main/java/com/nullstdio/bbfc2020/MainMainActivity.java
@@ -0,0 +1,129 @@
+package com.nullstdio.bbfc2020;
+
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.nullstdio.bbfc2020.FragmentAll.FixtureFragment;
+import com.nullstdio.bbfc2020.FragmentAll.GallaryFragment;
+import com.nullstdio.bbfc2020.FragmentAll.HistoryFragment;
+import com.nullstdio.bbfc2020.FragmentAll.HomeFragment;
+import com.nullstdio.bbfc2020.FragmentAll.TeamFragment;
+
+public class MainMainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    NavigationView navigationView;
+
+    private Fragment homeFragment;
+    private Fragment fixtureFragment;
+    private Fragment teamFragment;
+    private Fragment historyFragment;
+    private Fragment gallaryFragment;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_main);
+
+        toolbar =  findViewById(R.id.toolbarC);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawerlayout);
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this , drawerLayout , toolbar , R.string.start , R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+        navigationView.clearFocus();
+
+        //fragment
+
+        homeFragment = new HomeFragment();
+        fixtureFragment = new FixtureFragment();
+        teamFragment = new TeamFragment();
+        historyFragment = new HistoryFragment();
+        gallaryFragment = new GallaryFragment();
+
+
+        if (savedInstanceState == null){
+            setFragment(homeFragment);
+            navigationView.setCheckedItem(R.id.home_nav);
+        }
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+
+    private void setFragment(Fragment fragment) {
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragmentContainer , fragment);
+        transaction.commit();
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch(menuItem.getItemId()){
+
+            case R.id.home_nav :{
+                setFragment(homeFragment);
+                drawerLayout.closeDrawers();
+                break;
+            }
+            case R.id.fixture_nav :{
+                setFragment(fixtureFragment);
+                drawerLayout.closeDrawers();
+                break;
+            }
+            case R.id.team_nav :{
+                setFragment(teamFragment);
+                drawerLayout.closeDrawers();
+                break;
+            }
+            case R.id.history_nav :{
+                setFragment(historyFragment);
+                drawerLayout.closeDrawers();
+                break;
+            }
+            case R.id.gallery :{
+                setFragment(gallaryFragment);
+                drawerLayout.closeDrawers();
+                break;
+            }
+
+
+
+        }
+        return true;
+    }
+}
