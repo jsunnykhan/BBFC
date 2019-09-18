c3005b50432073e040ed2e524264ec68ab64d5f3
Asus <a.ekush045@gmail.com>
2019-09-19 03:27:34 +0600

    Teams Finished

--- app/src/main/java/com/nullstdio/bbfc2020/FragmentAll/PlayerInformationsFragment.java
+++ app/src/main/java/com/nullstdio/bbfc2020/FragmentAll/PlayerInformationsFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.nullstdio.bbfc2020.R;
 import com.squareup.picasso.Picasso;
@@ -18,6 +19,17 @@ public class PlayerInformationsFragment extends Fragment {
 
     private ImageView teamImage;
     private ImageView playerImage;
+    private TextView jersy1;
+    private TextView jersy2;
+    private TextView playerName1;
+    private TextView playerName2;
+    private TextView nickName;
+    private TextView yellow;
+    private TextView red;
+    private TextView positions;
+    private TextView goals;
+    private TextView assist;
+
 
     public PlayerInformationsFragment() {
 
@@ -32,6 +44,16 @@ public class PlayerInformationsFragment extends Fragment {
 
         teamImage = view.findViewById(R.id.teamImage);
         playerImage = view.findViewById(R.id.playerImage);
+        playerName1 = view.findViewById(R.id.playerName1);
+        playerName2 = view.findViewById(R.id.fullName);
+        jersy1 = view.findViewById(R.id.jersey1);
+        jersy2 = view.findViewById(R.id.jersey2);
+        nickName = view.findViewById(R.id.nickName);
+        positions = view.findViewById(R.id.playerPosition);
+        goals = view.findViewById(R.id.playerGoals);
+        assist = view.findViewById(R.id.playerAssist);
+        yellow = view.findViewById(R.id.yelloCard);
+        red = view.findViewById(R.id.redCard);
 
 
         Picasso.get().load("https://firebasestorage.googleapis.com/v0/b/bbfc-37a18.appspot.com/o/team_icon%2F20190915_045803.png?alt=media&token=d386be45-6dbf-4f4f-b5f7-89880019f0fd")
